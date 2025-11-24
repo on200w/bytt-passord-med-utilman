79f33f1d4ff7f120555c712002d262ed321b7ef9
Henrik S <hs-2k7@outlook.com>
2025-11-24 11:33:27 +0100

    v1.3

--- utilman.bat
+++ utilman.bat
@@ -3,7 +3,18 @@ chcp 65001 >nul
 title Utilman Passordverktøy
 color 0A
 
+:: Sett versjon og dato
+set versjon=1.3
+set dato=%date%
+set klokke=%time%
+
 :menu
+cls
+echo Tid: %klokke%
+echo Dato: %dato%
+echo Versjon: %versjon%   
+echo.
+
 echo ╔════════════════════════════════════════════════════════════╗
 echo ║              Utilman Passord-Reset Verktøy                 ║
 echo ╠════════════════════════════════════════════════════════════╣
@@ -15,10 +26,15 @@ echo ╚════════════════════════
 echo.
 set /p valg= Skriv inn valg (1-4): 
 
+:: Logg valget
+echo [%dato% %klokke%] Valg %valg% kjørt >> logg.txt
+
 if "%valg%"=="1" goto steg1
 if "%valg%"=="2" goto steg2
 if "%valg%"=="3" goto steg3
 if "%valg%"=="4" goto slutt
+echo Ugyldig valg. Prøv igjen.
+pause
 goto menu
 
 :steg1
@@ -27,8 +43,10 @@ copy c:\windows\system32\utilman.exe c:\windows\system32\utilman.exe.bak
 echo Erstatter utilman.exe med cmd.exe...
 copy c:\windows\system32\cmd.exe c:\windows\system32\utilman.exe
 echo Ferdig! Du kan starte maskinen på nytt manuelt når du vil.
+echo [%date% %time%] Valg 1: utilman.exe ble byttet med cmd.exe, backup lagret som utilman.exe.bak >> logg.txt
 pause
 goto menu
+
 
 :steg2
 echo Skriv inn brukernavn:
@@ -38,6 +56,7 @@ set /p nyttpassord=
 echo Endrer passord for %brukernavn% ...
 net user "%brukernavn%" "%nyttpassord%"
 echo Ferdig! Hvis ingen feilmelding vises, er passordet endret.
+echo [%date% %time%] Valg 2: Passord endret for bruker "%brukernavn%" >> logg.txt
 pause
 goto menu
 
@@ -45,8 +64,10 @@ goto menu
 echo Gjenoppretter original utilman.exe...
 copy c:\windows\system32\utilman.exe.bak c:\windows\system32\utilman.exe
 echo Ferdig! Du kan starte maskinen på nytt manuelt når du vil.
+echo [%date% %time%] Valg 3: utilman.exe ble gjenopprettet fra backup >> logg.txt
 pause
 goto menu
 
 :slutt
+echo [%date% %time%] Valg 4: Program avsluttet >> logg.txt
 exit
