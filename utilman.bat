bfbb747568e2369c94b7836ad37758296c127963
Henrik S <hs-2k7@outlook.com>
2025-11-26 11:41:08 +0100

    v2.1

--- utilman.bat
+++ utilman.bat
@@ -4,8 +4,7 @@ title Utilman Passordverktøy
 color 0A
 
 :: Versjon og klokke
-set versjon=2.0
-
+set versjon=2.1
 
 :: Sett loggsti til samme mappe som skriptet
 set "logPath=%~dp0logg.txt"
@@ -26,15 +25,17 @@ if not defined winDrive (
     exit
 )
 
-:loop
+:meny
 cls
 set klokke=%time:~0,8%
 set dato=%date%
 echo ╔══════════════════════════════╗
-echo ║ Tid:     %klokke%            ║
-echo ║ Dato:    %dato%          ║
-echo ║ Loggfil: %logPath%         ║
 echo ║ Versjon: %versjon%                 ║
+echo ╠══════════════════════════════╣
+echo ║ Tid:        %klokke%         ║
+echo ║ Dato:       %dato%       ║
+echo ║ Loggfil:    %logPath%      ║
+echo ║ WinStasjon: %winDrive%\system32      ║
 echo ╚══════════════════════════════╝
 echo.
 echo ╔═══════════════════════════════════════════════════════════════════════════╗
@@ -58,7 +59,7 @@ if "%valg%"=="2" goto steg2
 if "%valg%"=="3" goto steg3
 if "%valg%"=="4" goto steg4
 :: Hvis ugyldig eller tomt, vis meny på nytt
-goto loop
+goto meny
 
 :steg1
 echo Lager backup av utilman.exe...
@@ -66,7 +67,7 @@ copy "%winDrive%\Windows\System32\utilman.exe" "%winDrive%\Windows\System32\util
 copy "%winDrive%\Windows\System32\cmd.exe" "%winDrive%\Windows\System32\utilman.exe"
 echo [%date% %time%] Valg 1: utilman.exe ble byttet >> "%logPath%"
 pause
-goto loop
+goto meny
 
 :steg2
 set /p brukernavn=Brukernavn:
@@ -74,13 +75,13 @@ set /p nyttpassord=Nytt passord:
 net user "%brukernavn%" "%nyttpassord%"
 echo [%date% %time%] Valg 2: Passord endret for bruker "%brukernavn%" >> "%logPath%"
 pause
-goto loop
+goto meny
 
 :steg3
 copy "%winDrive%\Windows\System32\utilman.exe.bak" "%winDrive%\Windows\System32\utilman.exe"
 echo [%date% %time%] Valg 3: utilman.exe gjenopprettet i %winDrive% >> "%logPath%"
 pause
-goto loop
+goto meny
 
 :steg4
 echo [%date% %time%] Valg 4: Program avsluttet >> "%logPath%"
